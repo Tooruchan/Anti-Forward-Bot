d61b33d49be3b891e7a8a9bea7ca5138b558e88b
Tooruchan <tooruchan@protonmail.com>
2019-09-27 20:12:07 +0800

    Formatted code.

--- bot.py
+++ bot.py
@@ -16,10 +16,12 @@ def load_channels():
         f.close()
     _channels = a['channels']
 
+
 def load_conf():
     global _conf
-    with open("config.json",encoding="utf-8") as f:
+    with open("config.json", encoding="utf-8") as f:
         _conf = json.load(f)
+
 
 load_channels()
 load_conf()
@@ -30,11 +32,16 @@ api_id = _conf['api_id']
 print(api_id)
 api_hash = _conf['api_hash']
 print(api_hash)
-app = Client("bot",bot_token=token,api_id=str(api_id),api_hash=str(api_hash))
+app = Client("bot",
+             bot_token=token,
+             api_id=str(api_id),
+             api_hash=str(api_hash))
+
+
 # app = Client("bot",bot_token=token,api_id=str(api_id),api_hash=str(api_hash),proxy=dict(hostname="127.0.0.1",port=1080))
 @app.on_message(Filters.group)
 # 来自群组内部的消息
-def check_message(client,message):
+def check_message(client, message):
     try:
         channelid = message.forward_from_chat.id
     except:
@@ -42,10 +49,14 @@ def check_message(client,message):
     usrid = message.from_user.id
     usrfname = message.from_user.first_name
     if channelid in _channels:
-        client.delete_messages(chat_id=message.chat.id,message_ids=message.message_id)
-        client.send_message(chat_id=message.chat.id,text="["+usrfname+"](tg://user?id="+str(usrid)+") :\n"+"你刚才所转发的消息来源频道在本群不受允许。",parse_mode='markdown')
+        client.delete_messages(chat_id=message.chat.id,
+                               message_ids=message.message_id)
+        client.send_message(chat_id=message.chat.id,
+                            text="[" + usrfname + "](tg://user?id=" +
+                            str(usrid) + ") :\n" + "你刚才所转发的消息来源频道在本群不受允许。",
+                            parse_mode='markdown')
     else:
         print(message)
 
-app.run()
 
+app.run()
